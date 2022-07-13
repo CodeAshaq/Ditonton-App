277912262dd404bd5b475344e0bcba537f02f764
havizh as haq <19102056@ittelkom-pwt.ac.id>
2022-07-13 15:11:50 +0700

    bismillah

---
+++ core/lib/ssl/share.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+import 'package:flutter/services.dart';
+import 'package:http/io_client.dart';
+
+class Shared {
+  static Future<IOClient> initializeIOClient() async {
+    final ByteData sslCert =
+        await rootBundle.load('certificates/certificate.cer');
+    SecurityContext securityContext = SecurityContext(withTrustedRoots: false);
+    securityContext.setTrustedCertificatesBytes(sslCert.buffer.asInt8List());
+    HttpClient httpClient = HttpClient(context: securityContext);
+    httpClient.badCertificateCallback =
+        (X509Certificate cert, String host, int port) => false;
+    return IOClient(httpClient);
+  }
+}
--- core/lib/ssl/ssl_pining.dart
+++ core/lib/ssl/ssl_pining.dart
@@ -1,25 +1,10 @@
-// ignore: depend_on_referenced_packages
-import 'package:flutter/services.dart';
 import 'package:http/io_client.dart';
-// ignore: depend_on_referenced_packages
-import 'package:http/http.dart';
-import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:core/ssl/share.dart';
 
 class ApiIOClient extends IOClient {
-  Future<SecurityContext> get globalContext async {
-    final sslCert = await rootBundle.load('certificates/certificate.cer');
-    SecurityContext securityContext = SecurityContext(withTrustedRoots: false);
-    securityContext.setTrustedCertificatesBytes(sslCert.buffer.asInt8List());
-    return securityContext;
-  }
-
   @override
-  Future<Response> get(Uri url, {Map<String, String>? headers}) async {
-    HttpClient client = HttpClient(context: await globalContext);
-    client.badCertificateCallback =
-        (X509Certificate cert, String host, int port) => false;
-    IOClient ioClient = IOClient(client);
-
-    return ioClient.get(url);
+  Future<http.Response> get(Uri url, {Map<String, String>? headers}) async {
+    return await Shared.initializeIOClient().then((value) => value.get(url));
   }
 }
